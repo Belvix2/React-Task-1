5cbd77eb44926dca47dc33bace8432b9ee2863dd
Belvix <16433060+Belvix@users.noreply.github.com>
2023-10-14 23:17:10 +0530

    React task 1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import WordCounter from './WordCounter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Responsive Paragraph Word Counter</h1>
+      <WordCounter/>
+    </div>
+  );
+}
+
+export default App;
